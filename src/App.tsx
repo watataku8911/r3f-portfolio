2247e31f77b0266510592f57dfe015ec47da62c4
watataku8911 <watato.takuya@gmail.com>
2023-08-28 17:40:05 +0900

    ローディングアニメーションの追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,83 @@ import {
   Float,
   Html,
   PresentationControls,
-  Text,
   useGLTF,
 } from "@react-three/drei";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 2500);
+  }, [loading]);
+
   const macbook = useGLTF(
     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
   );
 
   return (
     <>
-      <Canvas camera={{ fov: 60, near: 0.1, far: 300 }}>
-        <color args={["#5bbee5"]} attach={"background"} />
-        <ambientLight />
-        <PresentationControls
-          global
-          config={{ mass: 2, tension: 400 }}
-          snap={{ mass: 4, tension: 300 }}
+      <div className="loading">
+        <div className="loading-dot">
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="shadow"></div>
+          <div className="shadow"></div>
+          <div className="shadow"></div>
+          <h6 className="loading-text">Loading</h6>
+        </div>
+      </div>
+      <div className={"contents " + (loading ? "loaded" : "")}>
+        <Canvas
+          camera={{ fov: 70, near: 1.1, far: 3000 }}
+          style={{ width: "100vw", height: "100vh" }}
         >
-          <Float rotationIntensity={2.5}>
-            <rectAreaLight
-              color={"#5bbee5"}
-              intensity={55}
-              rotation={[0.5, Math.PI, 0.2]}
-              width={2.0}
-              height={1.65}
-              position={[0, 0, -1]}
-            />
-            <primitive
-              object={macbook.scene}
-              position={[0.5, -1.3, 0.3]}
-              rotation-y={0.5}
-              rotation-x={0.5}
-            >
-              <Html
-                transform
-                wrapperClass="htmlScreen-pc"
-                distanceFactor={1.17}
-                position={[0, 1.56, -1.4]}
-                rotation-x={-0.256}
+          <color args={["#5bbee5"]} attach={"background"} />
+          <ambientLight />
+          <PresentationControls
+            global
+            config={{ mass: 2, tension: 400 }}
+            snap={{ mass: 4, tension: 300 }}
+          >
+            <Float rotationIntensity={1.5}>
+              <rectAreaLight
+                color={"#5bbee5"}
+                intensity={55}
+                rotation={[0.5, Math.PI, 0.2]}
+                width={2.0}
+                height={1.65}
+                position={[0, 0, -1]}
+              />
+              <primitive
+                object={macbook.scene}
+                position={[0.3, -1, 0.9]}
+                rotation-y={0.5}
+                rotation-x={0.5}
               >
-                <iframe src="https://watataku-portfolio.vercel.app" />
-              </Html>
-            </primitive>
-          </Float>
-        </PresentationControls>
-        <ContactShadows position-y={-2.0} opacity={0.7} scale={7} blur={2.4} />
-      </Canvas>
+                <Html
+                  transform
+                  wrapperClass="htmlScreen-pc"
+                  distanceFactor={1.17}
+                  position={[0, 1.56, -1.4]}
+                  rotation-x={-0.256}
+                >
+                  <iframe src="https://watataku-portfolio.vercel.app" />
+                </Html>
+              </primitive>
+            </Float>
+          </PresentationControls>
+          <ContactShadows
+            position-y={-2.0}
+            opacity={0.7}
+            scale={7}
+            blur={2.4}
+          />
+        </Canvas>
+      </div>
     </>
   );
 }
